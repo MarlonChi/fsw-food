191231a376c76cfd235289ff8bad38c2e64cf3c2
MarlonChi <marlonchiodelli@hotmail.com>
2024-04-29 20:50:25 -0300

    feat: header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { MenuIcon } from "lucide-react";
+
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Image src="/fsw-logo.png" alt="FSW Food" height={30} width={120} />
+      <Button
+        size="icon"
+        variant="secondary"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
